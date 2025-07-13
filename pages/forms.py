61b504b87581673e18c93d65b7abeddea8d7fd51
M_Haroun <mahmoudharoun2@gmail.com>
2025-07-13 02:47:13 +0200

    new updates in contact form

--- pages/forms.py
+++ pages/forms.py
@@ -6,3 +6,9 @@ class ContactForm(forms.ModelForm):
         model = Message
         #fields = '__all__'
         exclude = ['posted']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder':'Enter your full name'}),
+            'email': forms.EmailInput(attrs={'placeholder':'example@host.com'}),
+            'phone': forms.TextInput(attrs={'placeholder':'Enter your phone number'}),
+            'message': forms.Textarea(attrs={'placeholder':'Enter your message'}),
+        } 
